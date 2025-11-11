803673473fd2adcaeed09dd6e45dff6aae66c67f
Samir_Sherif <samir.sherif@student.giu-uni.de>
2025-11-11 09:42:26 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -3,17 +3,18 @@
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
-
-
-
-
-
-
-
-
-
-
-
-
+class Instructor:public Person {
+private:
+    string department;
+    int experienceYears;
+public:
+    Instructor();
+    Instructor(string n,int i,string dep,int ey);
+    ~Instructor();
+    void display();
+    string getDepartment();
+    int getExperienceYears();
+    void setDepartment(string d);
+    void setExperienceYears(int ey);
+};
 #endif
